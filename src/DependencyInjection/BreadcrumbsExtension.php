57e938d6c3ee563fa0d7c38990481213cf3717f5
Maciej Adamski <m.adamski@ideacode.pl>
2018-10-23 10:15:44 +0200

    Unnecessary configuration class has been removed

--- src/DependencyInjection/BreadcrumbsExtension.php
+++ src/DependencyInjection/BreadcrumbsExtension.php
@@ -13,9 +13,6 @@ class BreadcrumbsExtension extends Extension {
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container) {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . "/../Resources/config"));
         $loader->load("services.yaml");
     }
